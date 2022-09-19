d9e5cb63c7d648c77ef57e2ea0eb78210ecb060f
Max H <maxplaysbest@gmail.com>
2022-09-19 21:02:42 +1200

    idk

--- src/main/java/studio/tavex/thirdlife/ThirdLife.java
+++ src/main/java/studio/tavex/thirdlife/ThirdLife.java
@@ -1,6 +1,7 @@
 package studio.tavex.thirdlife;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import studio.tavex.thirdlife.listeners.DeathListener;
 import studio.tavex.thirdlife.listeners.JoinListener;
 
 public final class ThirdLife extends JavaPlugin {
@@ -16,11 +17,16 @@ public final class ThirdLife extends JavaPlugin {
 
         // Registering Events
         getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new DeathListener(), this);
 
+        // Yup this is working
+        System.out.println("Thirdlife Plugin not broken");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        System.out.println("Thirdlife plugin yup broken");
     }
 }
--- src/main/java/studio/tavex/thirdlife/listeners/DeathListener.java
+++ src/main/java/studio/tavex/thirdlife/listeners/DeathListener.java
@@ -1,6 +1,7 @@
 package studio.tavex.thirdlife.listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +16,21 @@ public class DeathListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e) {
             Player p = e.getEntity().getPlayer();
-            deathColourChanger.colourChangerScript(p);
+        int deaths = p.getStatistic(Statistic.DEATHS);
+        if (deaths >= 3){
+            p.setGameMode(GameMode.SPECTATOR);
+            p.sendTitle(ChatColor.RED + "You are dead", ChatColor.YELLOW + "GGWP", 10, 60, 10);
+            p.setDisplayName(ChatColor.RED + p.getDisplayName() + " (0)");
+        }
+        if (deaths == 2) {
+            p.setDisplayName(ChatColor.RED + p.getDisplayName() + " (1)");
+
+        }
+        if (deaths == 1) {
+            p.setDisplayName(ChatColor.YELLOW + p.getDisplayName() + " (2)");
+        }
+        if (deaths == 0) {
+            p.setDisplayName(ChatColor.GREEN + p.getDisplayName() + " (3)");
+        }
     }
 }
